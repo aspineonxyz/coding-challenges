039327d08f1e82e07326d794c247876d4afe82f1
Joshua Vernon <joshuatvernon@gmail.com>
2017-05-26 01:19:38 +1000

    Improved naive solution for Divisible Sum Pairs Hackerrank Question

--- Hackerrank/Algorithms/Implementation/7. Divisible Sum Pairs/divisible-sum-pairs-naive-solution.py
+++ Hackerrank/Algorithms/Implementation/7. Divisible Sum Pairs/divisible-sum-pairs-naive-solution.py
@@ -5,13 +5,10 @@ import sys
 def div_sum_pairs(arr, k) -> int:
   count = {}
   for i in range(len(arr)):
-    for j in range(len(arr)):
-      if i == j:
-        continue
+    for j in range(i+1, len(arr)):
       if (arr[i] + arr[j]) % k == 0:
         count[frozenset([i, j])] = 1
   return sum(count.values())
-
 
 n, k = map(int, input().strip().split(' '))
 a = list(map(int, input().strip().split(' ')))
